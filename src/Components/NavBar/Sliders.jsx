b8a0f8c5303273b22947be0c6f035a2abece4ff2
Sitaras <d.sitaras@icloud.com>
2022-12-10 01:59:24 +0200

    meta tag fb

--- src/Components/NavBar/Sliders.jsx
+++ src/Components/NavBar/Sliders.jsx
@@ -9,15 +9,31 @@ function Sliders({ children }) {
 
   return (
     <>
-    <Helmet>
-       <title>Sitaras</title>
-       <meta name="description" content="lalal" data-rh="true" />
-       <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://metatags.io/"/>
-        <meta property="og:title" content="Meta Tags — Preview, Edit and Generate"/>
-        <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"/>
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
-    </Helmet>
+      <Helmet>
+        <title>Sitaras</title>
+        <meta name="description" content="lalal" data-rh="true" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta
+          property="og:title"
+          content="Meta Tags — Preview, Edit and Generate"
+        />
+        <meta
+          property="og:description"
+          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="314" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta name="twitter:title" content="hello" />
+        <meta name="twitter:description" content="desc" />
+      </Helmet>
       {openMobileMenu && !desktopMenu ? (
         <MobileMenu />
       ) : search ? (
