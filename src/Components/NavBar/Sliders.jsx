59a57fbe4e7c936805fdebeece6481534d342804
Sitaras <d.sitaras@icloud.com>
2022-12-10 15:38:04 +0200

    abort meta tags

--- src/Components/NavBar/Sliders.jsx
+++ src/Components/NavBar/Sliders.jsx
@@ -2,38 +2,12 @@ import React from "react";
 import MobileMenu from "./MobileMenu/MobileMenu";
 import Search from "./Search/Search";
 import { useStateContext } from "../../Contexts/ContextProvider";
-import { Helmet  } from "react-helmet-async";
 
 function Sliders({ children }) {
   const { desktopMenu, openMobileMenu, search } = useStateContext();
 
   return (
     <>
-      <Helmet>
-        <title>Sitaras</title>
-        <meta name="description" content="lalal" data-rh="true" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
-        <meta
-          property="og:title"
-          content="Meta Tags — Preview, Edit and Generate"
-        />
-        <meta
-          property="og:description"
-          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
-        />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
-        <meta property="og:image:width" content="600" />
-        <meta property="og:image:height" content="314" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-
-        <meta name="twitter:title" content="hello" />
-        <meta name="twitter:description" content="desc" />
-      </Helmet>
       {openMobileMenu && !desktopMenu ? (
         <MobileMenu />
       ) : search ? (
--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ContextProvider } from "./Contexts/ContextProvider";
-import { HelmetProvider } from "react-helmet-async";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ContextProvider>
     <React.StrictMode>
-      <HelmetProvider>
         <App />
-      </HelmetProvider>
     </React.StrictMode>
   </ContextProvider>
 );
