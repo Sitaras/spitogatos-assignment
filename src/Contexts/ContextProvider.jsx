beeb57f3968dcdc59fd3c0c4dd47344285646abf
sitaras <sitaras@thinkdesquared.com>
2023-04-17 20:40:27 +0300

    style: note

--- src/Contexts/ContextProvider.jsx
+++ src/Contexts/ContextProvider.jsx
@@ -5,7 +5,7 @@ const StateContext = createContext();
 export const ContextProvider = ({ children }) => {
   const [screenSize, setScreenSize] = useState(window.innerWidth);
   const [desktopMenu, setDesktopMenu] = useState(true);
-  const [activeENGR, setActiveENGR] = useState("EN"); // (note: user preferable language should save it at the local host)
+  const [activeENGR, setActiveENGR] = useState("EN"); // (note: user's preferred language should be saved in local storage)
 
   return (
     <StateContext.Provider
